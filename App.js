3f4afd5b07d1a603f6b96a4edea1a04fcfc2c99b
tung <thanhtung230323@gmail.com>
2025-01-07 20:42:52 +0700

    hello

--- App.js
+++ App.js
@@ -1,88 +1,33 @@
-import React, { useState } from 'react';
-import { TouchableOpacity, StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
-
-  const [input,setinput]=useState("")
-  const [mylist,setmylist]=useState([])
-  const [array,setarray]=useState(    
-    [
-      { id: 1, ten: 'Tung', age: 20 },
-      { id: 2, ten: 'Hoa', age: 25 },
-      { id: 3, ten: 'Minh', age: 30 },
-      { id: 4, ten: 'Tung', age: 20 },
-      { id: 5, ten: 'Hoa', age: 25 },
-      { id: 6, ten: 'Minh', age: 30 },
-  
-    ]
-  )
+const App = () => {
   return (
-    
     <View style={styles.container}>
-    
-    <View style={styles.headingstlye}>
-      <View style={styles.headingstlye_1}>
-      <Text style={{color:"white" }}>To app</Text>
-      </View>
-      <View style={styles.buttonsyle}>
-      <Button  title='tung'></Button>
+      <View style={styles.square}>
+        <Text style={styles.text}>Hello, world</Text>
       </View>
     </View>
-    <View style={{ flexDirection: "row", height: 50 }}>
-  <Text style={{ backgroundColor: "grey", padding: 10,borderRadius:5 }}>Nhập</Text>
-  <TextInput
-    style={{
-      backgroundColor: "#f0f0f0",
-      padding: 10,
-      borderColor: "black",
-      borderWidth: 1,
-      flex: 1, // Cho phép TextInput chiếm phần còn lại
-      borderRadius: 5,
-    }}
-    placeholder="Nhập nội dung"
-    placeholderTextColor="gray"
-    onChangeText={(value) =>setinput(value)}
-
-  />
-   <TouchableOpacity style={{alignItems:"center",backgroundColor:"#4CAF50",justifyContent:"center"}} onPress={()=> alert("heloo")} >
-      <Text style={{textAlign:"center"}}>Search</Text>
-    </TouchableOpacity>
-</View>
-    <Text>to do:{input}</Text>
-  </View>
-
-  
-    
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-   
-    flex:1
-    
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  buttonsyle:{
-    alignSelf:"flex-end",
-    top:15,
-    width:40,
-    height:40,
-    fontSize:20
+  square: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  headingstlye:{
-    padding:20,
-    color:"white",
-    backgroundColor:"green",
-    alignItems:"center",
-    flex:0
-    
+  text: {
+    fontSize: 20,
+    color: 'white',
   },
- headingstlye_1:{
-  top:40,
-  width:50,
-  color:"white",
-  textAlign:"center",
-  justifyContent:"center"
- }
-
 });
+
+export default App;
