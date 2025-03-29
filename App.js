1024babf8fea348c722d0e8e12e3f9bdd40156b0
tung <thanhtung230323@gmail.com>
2025-03-29 15:18:46 +0700

    29/3

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
-import React from 'react';
-import AppNavigator from './navigation/AppNavigator';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import PaymentScreen from "./screens/PaymentScreen";
+import SuccessScreen from "./screens/SuccessScreen";
 
-export default function App() {
-  return <AppNavigator />;
-}
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Success" component={SuccessScreen} />
+        <Stack.Screen name="Payment" component={PaymentScreen} />
+       
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
